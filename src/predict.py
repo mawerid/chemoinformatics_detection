bedfabcb90a6de123d9a1b1e3ffa5fd7d81b3831
Max Mawerid <monasmax02@gmail.com>
2023-10-22 10:10:00 +0300

    Hotfix predict

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 
-def predict(data: np.ndarray) -> [float, float, float]:
+
+def predict(data: str) -> [float, float, float]:
     ic50 = np.random.ranf() * 10
     cc50 = np.random.ranf() * 10
     si = cc50 / ic50
-    return ic50, cc50 ,si
+    return ic50, cc50, si
